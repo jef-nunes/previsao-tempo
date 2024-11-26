33f218cad82b3e31670543cdfa859e915f7e7d03
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-11-26 17:33:25 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -21,14 +21,15 @@ async function gerarResultados()
     const chaveAPI = "";
     let nomeCidade = insereCidade.value;
     console.log(nomeCidade);
-    const dados = await fetch(
+    const res = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${nomeCidade}&appid=${chaveAPI}&units=metric&lang=pt_br`
     )
     .then(
         resposta=>resposta.json()
     );
-
-    console.log(dados);
+    displayCidade.textContent = `${res.name}`;
+    displayTemperatura.textContent = `Temperatura: ${Math.round(res.main.temp)}°C`;
+    displayTempo.textContent = `Tempo: ${res.weather[0].description}`;
 }
 
 btPesquisar.addEventListener("click", gerarResultados);
